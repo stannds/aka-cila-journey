25056b25a342ddaef4aa37e3e41fc7ea6f1d7cca
Samudra <3b098f@gmail.com>
2025-11-02 10:38:56 +0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App.jsx";
+import PuzzlePage from "./PuzzlePage.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <BrowserRouter basename="/">
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/:slug" element={<PuzzlePage />} />
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
